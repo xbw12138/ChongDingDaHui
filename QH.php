654ebdd69daba8835cc0170da19b2d7ecb4bb3c3
xbw12138 <xbw@ecfun.cc>
2018-01-12 19:52:55 +0800

    xbw
    很难受

--- QH.php
+++ QH.php
@@ -14,12 +14,12 @@ header('Content-type: application/json; charset=UTF-8');
 //$dirt='{"code":0,"msg":"成功","data":{"event":{"answerTime":10,"correctOption":0,"desc":"2.冯小刚导演的电影《芳华》，改编自谁的作品？","displayOrder":1,"liveId":98,"options":"[\"严歌苓\",\"张爱玲\",\"三毛\"]","questionId":1175,"showTime":1515733542277,"stats":[402096,90527,22795],"status":2,"type":"showAnswer"},"type":"showAnswer"}}';
 //$json=json_decode($dirt, true);
 //$ques_msg=$json['msg'];
+$order=1;
 while(true){
     //$dirt='{"code":0,"msg":"成功","data":{"event":{"answerTime":10,"desc":"11.动画片《哆啦a梦》中的胖虎是什么星座？","displayOrder":1,"liveId":98,"options":"[\"双子座\",\"白羊座\",\"狮子座\"]","questionId":1184,"showTime":1515734054248,"status":0,"type":"showQuestion"},"type":"showQuestion"}}';
     $dirt=getQuestion('http://htpmsg.jiecaojingxuan.com/msg/current');
     $json=json_decode($dirt, true);
     $ques_msg=$json['msg'];
-    $order=1;
     if($ques_msg=='no data'){
         sleep(1);
         echo ".";
