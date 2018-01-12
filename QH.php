c1921ef7cd9982f40d133677aab47cda99090dec
xbw12138 <xbw@ecfun.cc>
2018-01-12 16:41:46 +0800

    xbw
    init

---
+++ QH.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xubowen
+ * Date: 2018/1/12
+ * Time: 下午2:03
+ */
+header('Content-type: application/json; charset=UTF-8');
+//http://htpmsg.jiecaojingxuan.com/msg/current
+//{"code":0,"msg":"no data"}
+
+//$dirt='{"code":0,"msg":"成功","data":{"event":{"answerTime":10,"desc":"11.动画片《哆啦a梦》中的胖虎是什么星座？","displayOrder":10,"liveId":98,"options":"[\"双子座\",\"白羊座\",\"狮子座\"]","questionId":1184,"showTime":1515734054248,"status":0,"type":"showQuestion"},"type":"showQuestion"}}';
+$dirt=getQuestion('http://htpmsg.jiecaojingxuan.com/msg/current');
+//$dirt='{"code":0,"msg":"成功","data":{"event":{"answerTime":10,"correctOption":0,"desc":"2.冯小刚导演的电影《芳华》，改编自谁的作品？","displayOrder":1,"liveId":98,"options":"[\"严歌苓\",\"张爱玲\",\"三毛\"]","questionId":1175,"showTime":1515733542277,"stats":[402096,90527,22795],"status":2,"type":"showAnswer"},"type":"showAnswer"}}';
+$json=json_decode($dirt, true);
+$ques_msg=$json['msg'];
+//$T=10;
+while(true){
+    if($ques_msg=='no data'){
+        sleep(1);
+        echo "等待问题中……\n";
+    }else{
+        $type=$json['data']['type'];
+        if($type=='showAnswer'){
+            sleep(1);
+            echo "等待问题中……\n";
+        }else{
+            $ques_desc=$json['data']['event']['desc'];
+            $ques_options=$json['data']['event']['options'];
+            echo "-----------------------------\n";
+            echo $ques_desc."\n".$ques_options."\n";
+            echo "结果统计:\n";
+            $baiduAnswer=simpBaidu(getBaidu(formString($ques_desc)));
+            foreach (formOptions($ques_options) as &$select){
+                echo $select."       ".substr_count($baiduAnswer,$select)."\n";
+            }
+            echo "-----------------------------\n";
+            sleep(5);
+        }
+    }
+}
+//删除空格
+function trimall($str){
+    $oldchar=array(" ","　","\t","\n","\r");
+    $newchar=array("","","","","");
+    return str_replace($oldchar,$newchar,$str);
+}
+//格式化options
+function formOptions($str)
+{
+    $result = array();
+    preg_match_all("/(?:\[)(.*)(?:\])/i",$str, $result);
+    preg_match_all("#\"(.*?)\"#i",$result[1][0], $result);
+    return $result[1];
+}
+//获取问题
+function getQuestion($url){
+    $ch = curl_init();
+    $timeout = 5;
+    curl_setopt ($ch, CURLOPT_URL,$url);
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt ($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36');
+    curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    $html = curl_exec($ch);
+    curl_close ($ch);
+    return $html;
+}
+//精简百度搜索内容
+function simpBaidu($string){
+    $pattern = '/<div id="content_left">(.+?)<div style="clear:both;height:0;">/is';
+    preg_match($pattern, $string, $match);
+    return trimall($match[0]);
+}
+//获取百度搜索结果
+function getBaidu($desc){
+    $url = "http://www.baidu.com/s?wd=".$desc;
+    $ch = curl_init ();
+    $timeout = 5;
+    curl_setopt ($ch, CURLOPT_URL, $url);
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt ($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36');
+    curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    $content = curl_exec($ch);
+    curl_close ($ch);
+    return $content;
+}
+//格式化问题内容
+function formString($str){
+    $desc=$str;
+    $desc=chop($desc,"？");//去掉？
+    $desc=chop($desc,"?");//去掉?
+    $index=strpos($desc,".")+1;//获取.的位置
+    $res=substr($desc, $index);
+    //echo "格式化问题---".$res;
+    return $res; //截取.后的内容
+}
